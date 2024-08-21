a5c1c479fa899314c69d37ecd6806a7855fa4873
realqijun <ang.qijun@gmail.com>
2024-08-21 14:11:08 +0800

    add read input functionality

--- src/main/java/BabyGronk.java
+++ src/main/java/BabyGronk.java
@@ -1,24 +1,51 @@
 import javax.swing.*;
+import java.util.Scanner;
 
 public class BabyGronk {
-    private final static String seperators =  "💬💬💬💬💬💬💬💬💬💬💬💬💬💬💬💬💬💬💬💬💬💬💬💬💬💬💬";
+    private final static String seperators =  "💬💬💬💬💬💬💬💬💬💬💬💬💬💬💬💬💬💬💬💬💬💬💬💬💬💬💬\n";
 
     private static void logOff() {
         String goodByeMessage = seperators +
                 """
-                
                 Smell ya later.
                 """ +
                 seperators;
         System.out.println(goodByeMessage);
+        System.exit(0);
     }
+
     private static void greet() {
-        String welcomeMessage = """
+        String welcomeMessage = seperators +
+                """ 
                 What's up ohio? I'm BabyGronk, let's see how sigma you are
                 How much aura do you have?
-                """;
-        System.out.println(seperators + "\n" + welcomeMessage);
+                """ +
+                seperators;
+        System.out.println(welcomeMessage);
     }
+
+    private static String getPrompt() {
+        return new Scanner(System.in).nextLine();
+    }
+
+    private static String produceReply(String input) {
+        if (input == null || input.isEmpty()) {
+            return ("What bro?\n");
+        }
+        if (input.chars().allMatch(Character::isDigit)) {
+            int val = Integer.parseInt(input);
+            if (val < 1000) {
+                return ("That's too little! Don't talk to me until it's higher\n");
+            } else {
+                return ("Wow! Teach me how to be as sigma as you senpai\n");
+            }
+        }
+        if (input.equals("bye")) {
+            logOff();
+        }
+        return (input + "\n");
+    }
+
     public static void main(String[] args) {
         String logo = """
                 ⣠⣀⣤⣶⣶⣶⣶⣤⣤⣤⣤⣄⡀⠀⠀⠀⢀⣀⣀⣤⣤⣤⣶⣶⣶⣶⣬⣒⢦⡀
@@ -34,6 +61,9 @@ public class BabyGronk {
                 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀""";
         System.out.println("Up your rizz\n" + logo);
         greet();
-        logOff();
+        while (true) {
+            String answer = produceReply(getPrompt());
+            System.out.println(seperators + answer + seperators);
+        }
     }
 }
