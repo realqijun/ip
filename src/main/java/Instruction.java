0768dbe38e5f53f973bf545ba504b71353e42ffa
realqijun <ang.qijun@gmail.com>
2024-08-29 20:21:46 +0800

    Add more classes to adhere to OOP principles.
    Most of the program logic was stored in BabyGronk class, which
    did not adhere much to OOP style of coding.
    
    Parser is introduced to parse user input and produce Instruction
    that has the correct command as well as args formatted, otherwise,
    an InvalidInputException will be thrown.
    
    TaskList is created to store tasks and handle add and delete operations.
    
    Ui class handles the printing of strings to stdout as well as
    exiting the program.

---
+++ src/main/java/Instruction.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Instruction {
+    private String instruction;
+    private String[] args;
+
+    Instruction(String instruction, String[] args) {
+        this.instruction = instruction;
+        this.args = args;
+    }
+
+    public String getInstruction() {
+        return (instruction);
+    }
+
+    public String[] getArgs() {
+        return (Arrays.copyOfRange(args, 1, args.length));
+    }
+}
---
+++ src/main/java/TaskList.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class TaskList {
+    private List<Task> tasks;
+
+    TaskList(Stream<String> tasks) {
+        this.tasks = new ArrayList<>(100);
+        tasks.forEach((task) -> {this.tasks.add(BabyGronk.initTasks(task));});
+    }
+
+    public String delete(String[] input) {
+        StringBuilder builder = new StringBuilder();
+        for (String s : input) {
+            int toDelete = Integer.parseInt(s);
+            if (toDelete < 1 || toDelete > tasks.size()) {
+                if (tasks.isEmpty()) {
+                    return ("Ohio level brainrot detected, you haven't even added anything to the list\n");
+                } else {
+                    return ("Please pick within the range 1 - " + tasks.size() + "\n");
+                }
+            }
+            Task deleted = tasks.remove(toDelete - 1);
+            builder.append(deleted).append("\n has been ejected (they were the impostor)\n")
+                    .append(tasks.size()).append(" tasks remain\n");
+        }
+        return (builder.toString());
+    }
+
+    public String add(Task task) {
+        tasks.add(task);
+        return ("added task: " + tasks.get(tasks.size() - 1).toString() + "\n" +
+                tasks.size() + " task(s) in the list rn\n");
+    }
+
+    public String   markTask(String[] input, boolean status) {
+        StringBuilder builder = new StringBuilder();
+        for (String s : input) {
+            int toMark = Integer.parseInt(s);
+            if (toMark > tasks.size() || toMark < 1) {
+                return ("You got no rizz, try learning how to count (index " + toMark + " out of range)\n");
+            }
+            Task task = tasks.get(toMark - 1);
+            if (status == task.isDone()) {
+                return ("Task " + toMark + " is already " + (status ? "marked" : "unmarked") + "\n");
+            }
+            task.setDone(status);
+            if (status) {
+                builder.append("Let's go! +100 aura points!\n").append(task).append("\n");
+            } else {
+                builder.append("Bruh, you're cooked, -500 aura points\n").append(task).append("\n");
+            }
+        }
+        return (builder.toString());
+    }
+
+    public List<Task> getTasks() {
+        return (tasks);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < tasks.size(); i++) {
+            builder.append(i).append(".").append(tasks.get(i).display()).append("\n");
+        }
+        return (builder.toString());
+    }
+}
