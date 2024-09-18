3710e2d32f4f49127f30987fbee49d1ac0af98d1
realqijun <ang.qijun@gmail.com>
2024-09-18 19:51:37 +0800

    Attempt to fix StorageTest for CI
    CI compile fails for Windows. Error was in
    StorageTest.
    
    Use chatGPT to modify cleanup method in
    StorageTest.

--- src/test/java/seedu/babygronk/StorageTest.java
+++ src/test/java/seedu/babygronk/StorageTest.java
@@ -18,8 +18,8 @@ class StorageTest {
     void cleanUp(String filePath) throws IOException {
         File file = new File(filePath);
         if (file.exists()) {
-            Files.delete(file.toPath());
-            Files.delete(file.getParentFile().toPath());
+            Files.deleteIfExists(file.toPath());
+            Files.deleteIfExists(file.getParentFile().toPath());
         }
     }
 
